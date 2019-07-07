f304d9c0af54ef19d793d3e51ad0fa01ae598c63
Phirke Abhijeet <abhijeet.phirke@tieto.com>
2019-07-07 15:25:09 +0530

    With more fixes for sudirectory 'docportal'

---
+++ precache-manifest.8354ebbbd813f67cbc89b758068067dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "decbadd8bf030196e94defb38e6e29a4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "010227e7c75a4ee175c0",
+    "url": "./static/css/2.4146db7d.chunk.css"
+  },
+  {
+    "revision": "a88ca8df40db29dcb7b2",
+    "url": "./static/css/main.cc5d88d4.chunk.css"
+  },
+  {
+    "revision": "010227e7c75a4ee175c0",
+    "url": "./static/js/2.10451d74.chunk.js"
+  },
+  {
+    "revision": "a88ca8df40db29dcb7b2",
+    "url": "./static/js/main.eb70e9d8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7fd71338fc249237e96c3764082596e8.js"
+  "./precache-manifest.8354ebbbd813f67cbc89b758068067dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
